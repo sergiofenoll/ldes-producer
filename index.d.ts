e23bb8e1f00a26e024583f293d27865318cd8840
Nordine Bittich <contact@bittich.be>
2024-08-08 11:23:30 +0200

    remove generated files

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import Config from './models/config';
-import { AddDataOptions, GetNodeOptions } from './models/option';
-export declare function getNode(config: Config, options: GetNodeOptions): Promise<{
-    stream: NodeJS.ReadableStream;
-    fromCache: boolean;
-}>;
-export declare function addData(config: Config, options: AddDataOptions): Promise<void>;
